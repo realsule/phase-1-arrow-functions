56dd3582efff3953f90a54798c18a2088b5971e0
Suleiman <real1sule@gmail.com>
2025-09-06 07:03:49 +0300

    completed arrow functions

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
-// Function expression for divide
 const divide = function() {
   return 2000 / 100;
 };
 
-// Arrow function for square (takes 1 parameter)
 const square = x => x * x;
 
-// Arrow function for add (takes 2 parameters)
 const add = (a, b) => a + b;
